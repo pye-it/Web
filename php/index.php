68c54b5d3f77380b199091b666b337bed0e70514
pye-it <it@pye-motors.co.uk>
2026-02-24 21:09:56 +0000

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,12 +1,5 @@
 <?php
 
-$dsn = "mysql:host=db;dbname=testdb;charset=utf8mb4";
-$user = "testuser";
-$pass = "testpass";
+echo "hello world";
 
-try {
-    $pdo = new PDO($dsn, $user, $pass);
-    echo "Database connection successful";
-} catch (PDOException $e) {
-    echo "Connection failed";
-}
+?>
